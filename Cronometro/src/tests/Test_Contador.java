24863b4c01dccc4c0a1321425be4e14335ad1d0d
FedericoBorda <federicoborda3@hotmail.com>
2015-09-28 20:15:20 -0300

    Agregando programa Cronometro

---
+++ Cronometro/src/tests/Test_Contador.java
@@ -0,0 +1,25 @@
+package tests;
+
+import main.Cronometro;
+import junit.framework.*;
+
+public class Test_Contador extends TestCase {
+
+	@Test
+	public void test01(){
+		
+		Cronometro c = new Cronometro();
+		
+		for (int i=0; i < 5000; i++) {
+			  c.incrementarTiempo();
+			}
+		
+		int minutos = c.mostrarMinutos();
+		int segundos = c.mostrarSegundos();
+		
+		Assert.assertTrue(minutos==5);
+		Assert.assertTrue(segundos==3);
+	}
+	
+	
+}
